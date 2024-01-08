2a4638b272f0ef8ab9aacf2b63741d90a172e1dd
tariq7299 <tr.sar77an78@gmail.com>
2024-01-08 07:50:19 +0200

    working...

--- contacts-app/src/main.jsx
+++ contacts-app/src/main.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import ErrorPage from "./error-page";
-  import Contact, { loader as contactLoader,} from "./routes/contact"
+  import Contact, { loader as contactLoader, action as contactAction,} from "./routes/contact"
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact, {action as editAction,} from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
